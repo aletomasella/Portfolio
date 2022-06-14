170224ecc10dd663ef1ab747fe642f74c9f4bdd0
Alejandro Tomasella <aletomasella09@gmail.com>
2022-06-14 18:40:53 -0300

    Navbar social buttons and Home changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 import logo from "../assets/logo2.png";
 
 const Navbar = () => {
@@ -12,7 +14,7 @@ const Navbar = () => {
       <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
         {/* logo */}
         <div className="flex">
-          <img src={logo} alt="Logo" style={{ width: "60px" }} />
+          <img src={logo} alt="Logo" style={{ width: "70px" }} />
         </div>
         {/* menu */}
 
@@ -43,7 +45,50 @@ const Navbar = () => {
           <li className="py-6 text-4xl">Contact</li>
         </ul>
         {/* social icon */}
-        <div className="hidden"></div>
+        <div className="hidden lg:flex fixed flex-col top-[35%] left-2">
+          <ul>
+            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+              <a
+                className="flex justify-between items-center w-full text-gray-300"
+                href="https://www.linkedin.com/in/aletomasella/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                LinkedIn <FaLinkedin size={30} />
+              </a>
+            </li>
+            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-900">
+              <a
+                className="flex justify-between items-center w-full text-gray-300"
+                href="https://github.com/aletomasella"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github <FaGithub size={30} />
+              </a>
+            </li>
+            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-red-600">
+              <a
+                className="flex justify-between items-center w-full text-gray-300"
+                href="mailto:aletomasella09@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Email <HiOutlineMail size={30} />
+              </a>
+            </li>
+            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-600">
+              <a
+                className="flex justify-between items-center w-full text-gray-300"
+                href="https://drive.google.com/file/d/1M4FSaJtsH63E4qI_o7Nu8n4cboDx5SZU/view?usp=sharing"
+                target="_blank"
+                rel="noreferrer"
+              >
+                CV <BsFillPersonLinesFill size={30} />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
