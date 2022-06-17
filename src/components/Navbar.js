b5c65b47b63360476b2febee9694e0ec7c8a0fc6
Alejandro Tomasella <aletomasella09@gmail.com>
2022-06-17 09:50:54 -0300

    Almost done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import logo from "../assets/logo2.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -19,11 +20,31 @@ const Navbar = () => {
         {/* menu */}
 
         <ul className="hidden md:flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
         </ul>
 
         {/* hamburger */}
@@ -38,11 +59,31 @@ const Navbar = () => {
               : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
           }
         >
-          <li className="py-6 text-4xl">Home</li>
-          <li className="py-6 text-4xl">About</li>
-          <li className="py-6 text-4xl">Skills</li>
-          <li className="py-6 text-4xl">Work</li>
-          <li className="py-6 text-4xl">Contact</li>
+          <li className="py-6 text-4xl">
+            <Link onClick={handleNav} to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link onClick={handleNav} to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link onClick={handleNav} to="skills" smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link onClick={handleNav} to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link onClick={handleNav} to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
         </ul>
         {/* social icon */}
         <div className="hidden lg:flex fixed flex-col top-[35%] left-2">
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,4 +1,6 @@
 import React from "react";
+import LandingBragi from "../assets/LandingBragi.png";
+import LandingVideogamesAPP from "../assets/LandingVideogamesAPP.png";
 
 const Work = () => {
   return (
@@ -8,25 +10,73 @@ const Work = () => {
         className="w-full bg-[#0a192f] md:h-screen text-gray-300"
       >
         {/* container  */}
-        <div className="max-w-[1000px] max-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
           <div className="p-8">
             <p className="text-4xl font-bold border-b-4 border-orange-400">
               Work
             </p>
-            <p className="p-6">Check out some of my recent work</p>
+            <p className="p-6">// Check out some of my recent work</p>
           </div>
 
-          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-            <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
-              <div>
-                <span></span>
-                <div>
-                  <a href="/">
-                    <button></button>
+          <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
+            <div
+              style={{ backgroundImage: `url(${LandingBragi})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              {/* hover-effect  */}
+
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  Bragi App
+                </span>
+                <div className="pt-8 text-center">
+                  <a
+                    href="https://bragi-nine.vercel.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
                   </a>
 
-                  <a href="/">
-                    <button></button>
+                  <a
+                    href="https://github.com/martinvioli/Bragi"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div
+              style={{ backgroundImage: `url(${LandingVideogamesAPP})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              {/* hover-effect  */}
+
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  Videogames App
+                </span>
+                <div className="pt-8 text-center">
+                  {/* <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a> */}
+
+                  <a
+                    href="https://github.com/aletomasella/PI-Videogames"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
                   </a>
                 </div>
               </div>
