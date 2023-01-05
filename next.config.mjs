e039cb2c892b2993b71f1e8520f05fc12c5b8cfc
Alejandro Tomasella <aletomasella09@gmail.com>
2023-01-05 17:23:59 -0300

    Migrating to appDir in nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -12,6 +11,9 @@ const config = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
+  },
+  experimental: {
+    appDir: true,
   },
 };
 export default config;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Homepage = () => {
+  return <div className="text-xl text-red-600">Homepage</div>;
+};
+
+export default Homepage;
